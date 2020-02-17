1faa7471207037f588edc6c846934a7423a9fa7b
yaoyu bao <yaobao5354@gmail.com>
2020-02-17 06:22:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,13 @@ require 'pry'
 class Application
   def call(env)
     resp = Rack::Response.new
+    noon = Time.new(2015, 11, 27, 12, 00)
 
-
-    binding.pry
-    resp.write "Good Morning!"
-
+      if Time.now < noon
+        resp.write "Good Morning!"
+      else
+        resp.write "Good Afternoon!"
+      end
     resp.finish
   end
 end
